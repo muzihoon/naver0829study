6a86d89c8420ec2b42970a926edf63957f559fbb
muzihoon <youngsh92@gmail.com>
2023-08-30 11:45:50 +0900

    2일차커밋

---
+++ javawork/ArgsArray.java
@@ -0,0 +1,19 @@
+public class ArgsArray
+{
+	public static void main(String []args)
+	{
+		//args 로 두개의 숫자를 읽어서 출력해보자
+		String str1=args[0];
+		String str2=args[1];
+		System.out.println("str1="+str1);
+		System.out.println("str2="+str2);
+		System.out.println("두수의 합="+(str1+str2));
+
+		//String을 기본형 int 로 변환하는 방법
+		// int su1=(int) 이건 기본형 끼리만 된다.
+		//Integer 라는 Wrapper class를 이용해서 변환해보자
+		int su1=Integer.parseInt(str1);
+		int su2=Integer.parseInt(str2);
+		System.out.println("두수의 합="+(su1+su2));		
+	}
+}
